22e3010146dec09bdb9fcac3d5f4fdcccba3fe4f
Jinji Wei <jinjiwei@Jinjis-MacBook-Pro.local>
2019-06-14 11:22:16 +0800

    初始化

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="jumbotron-heading text-center">
+        1
+      </h1>
+      <p className="text-center">
+        <button className="btn btn-primary mr-2">Increase</button>
+        <button className="btn btn-danger my-2">Decrease</button>
+      </p>
     </div>
   );
 }
