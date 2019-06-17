580e6e7a3250d85ef95735a7fffd96e319bea776
Jinji Wei <jinjiwei@Jinjis-MacBook-Pro.local>
2019-06-17 11:01:23 +0800

    使用react-redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import PropsTypes from 'prop-types';
+import PropTypes from 'prop-types';
+// 在App种获取store的第三种方法
+import { connect } from 'react-redux';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="jumbotron-heading text-center">
-          {this.props.value}
+          {/* {this.context.store.getState()} 在App组件中获取store的第二种方法 */}
+          {this.props.counter}
         </h1>
         <p className="text-center">
-          <button onClick={this.props.onIncrement} className="btn btn-primary mr-2">Increase</button>
-          <button onClick={this.props.onDecreament} className="btn btn-danger my-2">Decrease</button>
+          <button className="btn btn-primary mr-2">Increase</button>
+          <button className="btn btn-danger my-2">Decrease</button>
         </p>
       </div>
     )
   }
 }
-App.protoTypes = {
-  value: PropsTypes.number.isRequired,
-  onIncrement: PropsTypes.func.isRequired,
-  onDecreament: PropsTypes.func.isRequired,
+// 在App组件中获取store的第二种方法
+// App.contextType = {
+//   store: PropTypes.object,
+// }
+
+const mapStateToProps = (state) => { // 把state转成props的形式
+  // 这里的state相当于store.getState()
+  return {
+    counter: state
+  }
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers/counter'
@@ -9,15 +10,7 @@ import { increament, decreament } from './actions'
 const store = createStore(reducer)
 // store.subscribe(() => console.log("State updated", store.getState()))
 
-const render = () => {
-    ReactDOM.render(<App
-        value={ store.getState() }
-        onIncrement={ () => store.dispatch(increament()) }
-        onDecreament={ () => store.dispatch(decreament()) }
-    />, document.getElementById('root'));
-}
-
-render()
-store.subscribe(render)
+// ReactDOM.render(<Provider store={store}><App store={store} /></Provider>, document.getElementById('root')); // 这里不提倡直接在App组件上传入store 当组件很多的时候就不是很方便
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
